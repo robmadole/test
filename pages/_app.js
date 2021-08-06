375f3268500d03e9344fcb44a84fb7ff7c442e19
robmadole <robmadole@gmail.com>
2021-08-06 09:50:14 -0500

    Install FA CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 import 'tailwindcss/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
